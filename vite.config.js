e4d6058308141b4d2fd7a0ac9463ce57565b139b
Junior Balcazar - Judev <junior.marketing@selk.es>
2026-03-13 14:30:16 +0100

    refactor(img): update the icons for the PWA

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,12 @@ export default defineConfig({
     vueDevTools(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'secretly_logo.png', 'secretly_dark_logo.png', 'secretly_light_logo.png'],
+      includeAssets: [
+        'favicon.ico',
+        'secretly_logo.png',
+        'secretly_dark_logo.png',
+        'secretly_light_logo.png',
+      ],
       manifest: {
         name: 'Secretly',
         short_name: 'Secretly',
@@ -25,17 +30,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/secretly_logo.png',
+            src: '/secretly_dark_logo.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/secretly_logo.png',
+            src: '/secretly_dark_logo.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/secretly_logo.png',
+            src: '/secretly_dark_logo.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
